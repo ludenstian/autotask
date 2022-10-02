9e2dac87d361505e9e51b2372351bd1a2a8d21a7
nhathuy <nhathuy13598@gmail.com>
2022-10-02 10:50:39 +0700

    Improve extension

--- src/extension.ts
+++ src/extension.ts
@@ -5,12 +5,10 @@ import { AutomataskProvider } from './util/provider';
 export async function activate(context: vscode.ExtensionContext) {
     let providerDisposable = vscode.tasks.registerTaskProvider(AutomataskProvider.AUTOMATASK_TYPE,
         new AutomataskProvider());
-    let tasks:vscode.Task[] = await vscode.tasks.fetchTasks();
     let commandDisposable = vscode.commands.registerCommand(AutomataskCommand.AUTOMATASK_COMMAND, async () => {
+        let tasks: vscode.Task[] = await vscode.tasks.fetchTasks();
         let command = new AutomataskCommand(tasks, context);
         await command.run();
     });
     context.subscriptions.push(providerDisposable, commandDisposable);
 }
-
-export function deactivate() { }
--- src/util/provider.ts
+++ src/util/provider.ts
@@ -1,28 +1,37 @@
+import { readFileSync } from 'fs';
+import path = require('path');
 import * as vscode from 'vscode';
 
+export interface Requirement {
+    scriptToCheck: string;
+    expectedValue: string;
+    name: string;
+    useTask: boolean;
+}
 export interface AutomataskDefinition extends vscode.TaskDefinition {
     filetype: string;
-    validate?: string;
-    expected?: string;
-    trigger: string;
+    filename: string;
+    taskToTrigger: string;
+    require?: Array<Requirement>;
 }
 
 export class AutomataskProvider implements vscode.TaskProvider {
     // eslint-disable-next-line @typescript-eslint/naming-convention
     public static AUTOMATASK_TYPE = "automatask";
 
-    private _tasks: vscode.Task[] | undefined;
-
-    constructor() {
-    }
-
     public provideTasks(token: vscode.CancellationToken): vscode.ProviderResult<vscode.Task[]> {
-        return undefined;
+        return [];
     }
 
     public resolveTask(task: vscode.Task, token: vscode.CancellationToken): vscode.ProviderResult<vscode.Task> {
-        const taskDefinition: AutomataskDefinition = <any>task.definition;
+        const taskDefinition = this.parseTaskInfo(task);
         return new vscode.Task(taskDefinition, vscode.TaskScope.Workspace,
-            task.name, AutomataskProvider.AUTOMATASK_TYPE, new vscode.ShellExecution(taskDefinition.validate!));
+            task.name, AutomataskProvider.AUTOMATASK_TYPE, new vscode.ShellExecution("We don't use this object"));
+    }
+
+    public parseTaskInfo(task: vscode.Task): AutomataskDefinition {
+        let taskDefinition: AutomataskDefinition = <any>task.definition;
+        taskDefinition.name = task.name;
+        return taskDefinition;
     }
 }
